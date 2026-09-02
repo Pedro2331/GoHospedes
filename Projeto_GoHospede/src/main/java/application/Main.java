937d7b8da6032847bc9cc5707b3842506096c03f
aluno <aluno@REF47338LAB1PC.edu.ce.senac.br>
2026-09-02 18:42:32 -0300

    commit inicial

---
+++ Projeto_GoHospede/src/main/java/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {    
+    
+    @Override
+    public void start(Stage janela) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/application/TelaControle.fxml"));
+        Scene cena = new Scene(root,1920,1080); 
+        janela.setScene(cena);
+        janela.setTitle("Tela Senac");
+        janela.setMaximized(false);
+        janela.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
---
+++ Projeto_GoHospede/src/main/java/application/TelaControleCadastro.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class TelaControleCadastro {
+
+    @FXML
+    private Pane Pbemvindo;
+
+    @FXML
+    private ImageView Tela;
+
+}
